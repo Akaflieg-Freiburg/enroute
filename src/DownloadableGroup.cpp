7bf4aee15e8be4d1ac2277f1edd3d346ed7f603c
Stefan Kebekus <stefan.kebekus@math.uni-freiburg>
2020-03-07 21:57:10 +0100

    Skeleton for Downloadable group

---
+++ src/DownloadableGroup.cpp
@@ -0,0 +1,65 @@
+/***************************************************************************
+ *   Copyright (C) 2019-2020 by Stefan Kebekus                             *
+ *   stefan.kebekus@gmail.com                                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "DownloadableGroup.h"
+
+
+DownloadableGroup::DownloadableGroup(QObject *parent)
+    : QObject(parent), _cachedDownloading(false), _cachedUpdatable(false)
+{
+#warning not implemented
+    ;
+}
+
+
+DownloadableGroup::~DownloadableGroup()
+{
+#warning not implemented
+    ;
+}
+
+
+void DownloadableGroup::addToGroup(Downloadable *downloadable)
+{
+#warning not implemented
+    ;
+}
+
+
+void DownloadableGroup::removeFromGroup(Downloadable *downloadable)
+{
+#warning not implemented
+    ;
+}
+
+
+bool DownloadableGroup::downloading() const
+{
+#warning not implemented
+    return false;
+
+}
+
+
+bool DownloadableGroup::updatable() const
+{
+#warning not implemented
+    return false;
+}

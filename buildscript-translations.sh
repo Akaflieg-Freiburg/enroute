8064549a538c547657c673389b0ec55acdd29c2c
Stefan Kebekus <stefan.kebekus@math.uni-freiburg>
2020-06-25 07:32:15 +0200

    Add French translation

--- buildscript-translations.sh
+++ buildscript-translations.sh
@@ -4,7 +4,8 @@ cd ../enrouteText
 git pull
 lupdate-qt5 ../enroute/src -ts \
 	    assets/enroute_cz.ts \
-	    assets/enroute_de.ts
+	    assets/enroute_de.ts \
+	    assets/enroute_fr.ts
 git commit -a -m "Update translations"
 git push
 

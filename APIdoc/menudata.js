545252a596dfb801c382135405eb29a5709fdfbc
kebekus <kebekus@users.noreply.github.com>
2020-09-27 13:01:19 +0000

    Deploying to gh-pages from  @ e5d9b4fa0eaed5f9d55b53acec080d87831d61af 🚀

--- APIdoc/menudata.js
+++ APIdoc/menudata.js
@@ -67,6 +67,7 @@ var menudata={children:[
 {text:"l",url:"functions_prop.html#index_l"},
 {text:"m",url:"functions_prop.html#index_m"},
 {text:"n",url:"functions_prop.html#index_n"},
+{text:"o",url:"functions_prop.html#index_o"},
 {text:"p",url:"functions_prop.html#index_p"},
 {text:"q",url:"functions_prop.html#index_q"},
 {text:"r",url:"functions_prop.html#index_r"},

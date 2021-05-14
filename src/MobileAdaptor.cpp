5f3ea8ec50f0f26e59beb0cd25f267691170be1e
Stefan Kebekus <stefan.kebekus@math.uni-freiburg>
2021-05-14 08:11:19 +0200

    Fix file name

--- src/MobileAdaptor.cpp
+++ src/MobileAdaptor.cpp
@@ -184,7 +184,6 @@ void MobileAdaptor::showDownloadNotification(bool show)
     if (show) {
         if (downloadNotification.isNull()) {
             downloadNotification = new KNotification(QStringLiteral("downloading"), KNotification::Persistent, this);
-            downloadNotification->setComponentName("enroute");
             downloadNotification->setPixmap( {":/icons/appIcon.png"} );
             downloadNotification->setText(tr("Downloading map data…"));
         }

ab1a548f76c44bf773c6957c45e7e29c63e0ed44
Stefan Kebekus <stefan.kebekus@math.uni-freiburg>
2021-08-17 14:29:13 +0200

    Fix compilation issue

--- src/traffic/TrafficDataSource_Abstract_GDL90.cpp
+++ src/traffic/TrafficDataSource_Abstract_GDL90.cpp
@@ -18,6 +18,8 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <array>
+
 #include "positioning/Geoid.h"
 #include "positioning/PositionProvider.h"
 #include "traffic/TrafficDataSource_Abstract.h"

bdac7359c267d3ad2a5b618053dab2299b2c2b66
Stefan Kebekus <stefan.kebekus@math.uni-freiburg>
2021-12-09 07:51:29 +0100

    Implement rudimentary desktop notifications

--- src/platform/Notifier_Linux.cpp
+++ src/platform/Notifier_Linux.cpp
@@ -18,126 +18,90 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <QApplication>
 #include <QDBusInterface>
-#include <QDebug>
+#include <QDBusReply>
 
 #include "platform/Notifier.h"
 
 
-QMap<Platform::Notifier::Notifications, uint> notificationIDs;
+QMap<Platform::Notifier::NotificationTypes, uint> notificationIDs;
+QPointer<QDBusInterface> notficationInterface;
+
+// This implementation of notifications uses the specification found here:
+// https://specifications.freedesktop.org/notification-spec/latest/ar01s09.html
+//
+// Help with DBus programming is found here:
+// https://develop.kde.org/docs/d-bus/accessing_dbus_interfaces/
+
+
+
+// This method returns a pointer to the QDBusInterface used to access notifications.
+// It constructs the object, if necessary.
+// We do not implement this in the constructor because the construction
+// takes time, which would add to the startup delay.
+
+QDBusInterface* getNotificationInterface()
+{
+    if (notficationInterface.isNull()) {
+        notficationInterface = new QDBusInterface("org.freedesktop.Notifications",
+                                                  "/org/freedesktop/Notifications",
+                                                  "org.freedesktop.Notifications",
+                                                  QDBusConnection::sessionBus());
+        notficationInterface->setParent(qApp);
+    }
+    return notficationInterface;
+}
 
 
 Platform::Notifier::Notifier(QObject *parent)
     : QObject(parent)
 {
-    ;
 }
 
 
 Platform::Notifier::~Notifier()
 {
-    QDBusInterface notify("org.freedesktop.Notifications",
-                          "/org/freedesktop/Notifications",
-                          "org.freedesktop.Notifications",
-                          QDBusConnection::sessionBus());
 
     foreach(auto notificationID, notificationIDs) {
         if (notificationID == 0) {
             continue;
         }
-        auto reply = notify.call("CloseNotification", notificationID); // time: 0 means: never expire.
-        if (reply.type() != QDBusMessage::ReplyMessage) {
-            qWarning() << reply.errorName() << reply.errorMessage();
-        }
+        getNotificationInterface()->call("CloseNotification", notificationID);
     }
+    delete notficationInterface;
 
 }
 
 
-void Platform::Notifier::hideNotification(Platform::Notifier::Notifications notificationType)
+void Platform::Notifier::hideNotification(Platform::Notifier::NotificationTypes notificationType)
 {
-qWarning() << "hide" << notificationType;
-    if (!notificationIDs.contains(notificationType)) {
-        return;
-    }
 
     auto notificationID = notificationIDs.value(notificationType, 0);
     if (notificationID == 0) {
         return;
     }
 
-    QDBusInterface notify("org.freedesktop.Notifications",
-                          "/org/freedesktop/Notifications",
-                          "org.freedesktop.Notifications",
-                          QDBusConnection::sessionBus());
-    auto reply = notify.call("CloseNotification", notificationID); // time: 0 means: never expire.
-    if (reply.type() != QDBusMessage::ReplyMessage) {
-        qWarning() << reply.errorName() << reply.errorMessage();
-    }
+    getNotificationInterface()->call("CloseNotification", notificationID);
     notificationIDs.remove(notificationType);
 
 }
 
 
-void Platform::Notifier::showNotification(Notifications notification, const QString& text, const QString& longText)
+void Platform::Notifier::showNotification(NotificationTypes notificationType, const QString& text, const QString& longText)
 {
-    qWarning() << "show" << notification;
 
-    QDBusInterface notify("org.freedesktop.Notifications",
-                          "/org/freedesktop/Notifications",
-                          "org.freedesktop.Notifications",
-                          QDBusConnection::sessionBus());
-
-    auto reply = notify.call("Notify",
-                             "Enroute Flight Navigation",
-                             notificationIDs.value(notification, 0), // Notification to replace. 0 means: do not replace
-                             "de.akaflieg_freiburg.enroute",
-                             title(notification),
-                             text,
-                             QStringList(), // actions_list
-                             QMap<QString,QVariant>(), // hint
-                             0); // time: 0 means: never expire.
-    if (reply.type() == QDBusMessage::ReplyMessage) {
-        if (reply.arguments().size() > 0) {
-            bool ok = false;
-            auto number = reply.arguments().at(0).toUInt(&ok);
-            if (ok) {
-                notificationIDs.insert(notification, number);
-            }
-        }
-    } else {
-        qWarning() << reply.errorName() << reply.errorMessage();
+    QDBusReply<uint> reply = getNotificationInterface()->call("Notify",
+                                                              "Enroute Flight Navigation",
+                                                              notificationIDs.value(notificationType, 0), // Notification to replace. 0 means: do not replace
+                                                              "", // Icon
+                                                              "Enroute Flight Navigation", // Title
+                                                              title(notificationType), // Summary
+                                                              QStringList(), // actions_list
+                                                              QMap<QString,QVariant>(), // hint
+                                                              0); // time: 0 means: never expire.
+    if (reply.isValid()) {
+        notificationIDs.insert(notificationType, reply.value());
     }
 
-    /*
-    // Get notificonst cation, &if it exists; otherwise get nullptr
-    auto notificationPtr = notificationPtrs.value(notification, nullptr);
-
-    // Otherwise, generate a new notification
-    if (notificationPtr.isNull()) {
-        switch (notification) {
-        case DownloadInfo:
-            notificationPtr = new KNotification(QStringLiteral("downloading"), KNotification::Persistent, this);
-            break;
-        case TrafficReceiverSelfTestError:
-            notificationPtr = new KNotification(QStringLiteral("trafficReceiverSelfTestError"), KNotification::Persistent, this);
-            break;
-        case TrafficReceiverRuntimeError:
-            notificationPtr = new KNotification(QStringLiteral("trafficReceiverRuntimeError"), KNotification::Persistent, this);
-            break;
-        }
-        notificationPtr->setDefaultAction( tr("Open Application") );
-        notificationPtr->setPixmap( {":/icons/appIcon.png"} );
-    }
-
-    notificationPtr->setTitle(title(notification));
-    notificationPtrs[notification] = notificationPtr;
-    connect(notificationPtr, &KNotification::defaultActivated, [this, notification]() { emit notificationClicked(notification); });
-    if (longText.isEmpty()) {
-        notificationPtr->setText(text);
-    } else {
-        notificationPtr->setText(longText);
-    }
-    notificationPtr->sendEvent();
-*/
 }

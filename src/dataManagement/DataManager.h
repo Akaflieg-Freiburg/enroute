4250ac4434f123f965ff6045b06962fb171bbb0c
Stefan Kebekus <stefan.kebekus@math.uni-freiburg>
2022-05-05 07:50:26 +0200

    API overhaul

--- src/dataManagement/DataManager.h
+++ src/dataManagement/DataManager.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2019-2021 by Stefan Kebekus                             *
+ *   Copyright (C) 2019-2022 by Stefan Kebekus                             *
  *   stefan.kebekus@gmail.com                                              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -20,8 +20,6 @@
 
 #pragma once
 
-#include <QTimer>
-
 #include "GlobalObject.h"
 #include "dataManagement/DownloadableGroup.h"
 
@@ -29,92 +27,119 @@
 namespace DataManagement {
 
 /*! \brief Manages the list of geographic maps
-
-  This class manages a list of available and installed geographic maps.  It
-  retrieves the list of geographic maps from a remote server on a regular basis,
-  updates the list automatically once a week, and maintains a list of
-  Downloadable objectes that correspond to these geographic maps.  It informs
-  the user if updates are available for one or several of these geographic maps.
-
-  The list of available maps is downloaded from a remote server whose address is
-  hardcoded into the binary. The list is downloaded to a file "maps.json" in
-  QStandardPaths::writableLocation(QStandardPaths::AppDataLocation). The format
-  of the file is described at this URL:
-  https://github.com/Akaflieg-Freiburg/enrouteServer/wiki/The-file-maps.json
-
-  Those geographic maps that are downloaded will be installed into the directory
-  "aviation_maps" in
-  QStandardPaths::writableLocation(QStandardPaths::AppDataLocation), or into a
-  suitable subdirectory of this.
-
-  This class assumes that no other program interferes with the file "maps.json"
-  and with the directory "aviation_maps".
-*/
+ *
+ *  This class manages a list of available and installed geographic maps.  It
+ *  retrieves the list of geographic maps from a remote server on a regular basis,
+ *  updates the list automatically once a week, and maintains a list of
+ *  Downloadable objects that correspond to these geographic maps.  It informs
+ *  the user if updates are available for one or several of these geographic maps.
+ *
+ *  The list of available maps is downloaded from a remote server whose address is
+ *  hardcoded into the binary. The list is downloaded to a file "maps.json" in
+ *  QStandardPaths::writableLocation(QStandardPaths::AppDataLocation). The format
+ *  of the file is described at this URL:
+ *  https://github.com/Akaflieg-Freiburg/enrouteServer/wiki/The-file-maps.json
+ *
+ *  Those geographic maps that are downloaded will be installed into the directory
+ *  "aviation_maps" in QStandardPaths::writableLocation(QStandardPaths::AppDataLocation), or into a
+ *  suitable subdirectory of this.
+ *
+ *  This class assumes that no other program interferes with the file "maps.json"
+ *  and with the directory "aviation_maps".
+ */
 
 class DataManager : public GlobalObject
 {
-  Q_OBJECT
-  
+    Q_OBJECT
+
 public:
-  /*! \brief Standard constructor
-
-    This constructor reads the file "maps.json" and initiates a download of the
-    file if no file is available or if the last download is more than one week
-    old.
-
-    @param parent The standard QObject parent pointer.
-  */
-  explicit DataManager(QObject *parent=nullptr);
+    /*! \brief Standard constructor
+     *
+     *  This constructor reads the file "maps.json" and initiates a download of the
+     *  file if no file is available or if the last download is more than one week
+     *  old.
+     *
+     *  @param parent The standard QObject parent pointer.
+     */
+    explicit DataManager(QObject *parent=nullptr);
 
     // deferred initialization
     void deferredInitialization();
 
-  /*! \brief Destructor
 
-    This destructor purges the download directory "aviation_map", by deleting
-    all files that do not belong to any of the maps.
-  */
-  void cleanUp();
-  
-  /*! \brief Pointer to the DownloadableGroup that holds all aviation maps
+    //
+    // PROPERTIES
+    //
 
-    The is a DownloadableGroupWatcher that holds all aviation maps. The maps
-    also appear in geoMaps, which is the union of aviation maps and base maps.
-  */
-  Q_PROPERTY(DataManagement::DownloadableGroupWatcher *aviationMaps READ aviationMaps CONSTANT)
+    /*! \brief Pointer to a DownloadableGroupWatcher that holds all aviation maps
+     *
+     *  Pointer to a DownloadableGroupWatcher that holds all aviation maps. The maps
+     *  also appear in geoMaps, which is the union of aviation maps and base maps.
+     */
+    Q_PROPERTY(DataManagement::DownloadableGroupWatcher* aviationMaps READ aviationMaps CONSTANT)
 
-  /*! \brief Getter function for the property with the same name
-    
+    /*! \brief Pointer to a DownloadableGroupWatcher that holds all base maps
+     *
+     *  Pointer to a DownloadableGroupWatcher that holds all base maps. The maps also appear in
+     *  geoMaps, which is the union of aviation maps and base maps.
+     */
+    Q_PROPERTY(DataManagement::DownloadableGroupWatcher* baseMaps READ baseMaps CONSTANT)
+
+    /*! \brief Pointer to a DownloadableGroupWatcher that holds all data items
+     *
+     *  Pointer to a DownloadableGroupWatcher that holds all data items.
+     */
+    Q_PROPERTY(DataManagement::DownloadableGroupWatcher* databases READ databases CONSTANT)
+
+    /*! \brief Indicates whether the file "maps.json" is currently being downloaded */
+    Q_PROPERTY(bool downloadingGeoMapList READ downloadingGeoMapList NOTIFY downloadingGeoMapListChanged)
+
+    /*! \brief Pointer to a DownloadableGroupWatcher that holds all geographic maps
+     *
+     *  Pointer to a DownloadableGroupWatcher that holds all maps.  This is the union of
+     *  aviation maps and base maps.
+     */
+#warning check if correct. Are data items also included?
+    Q_PROPERTY(DataManagement::DownloadableGroupWatcher* geoMaps READ geoMaps CONSTANT)
+
+    /*! \brief True if the list of available geo maps has already been downloaded */
+#warning also includes data items. might want to change name
+    Q_PROPERTY(bool hasGeoMapList READ hasGeoMapList NOTIFY geoMapListChanged)
+
+    /*! \brief Current "what's new" message */
+    Q_PROPERTY(QString whatsNew READ whatsNew NOTIFY whatsNewChanged)
+
+    /*! \brief Hash of the current "what's new" message */
+    Q_PROPERTY(uint whatsNewHash READ whatsNewHash NOTIFY whatsNewChanged)
+
+    // ------------------------------------------
+
+    /*! \brief Destructor
+     *
+     *  This destructor purges the download directory "aviation_map", by deleting
+     *  all files that do not belong to any of the maps.
+     */
+    void cleanUp();
+
+    /*! \brief Getter function for the property with the same name
+
     @returns Property aviationMaps
   */
-  DataManagement::DownloadableGroupWatcher *aviationMaps() { return &_aviationMaps; }
-  
-  /*! \brief Pointer to the DownloadableGroup that holds all base maps
+    DataManagement::DownloadableGroupWatcher *aviationMaps() { return &_aviationMaps; }
 
-    The is a DownloadableGroup that holds all base maps. The maps also appear in
-    geoMaps, which is the union of aviation maps and base maps.
-  */
-  Q_PROPERTY(DataManagement::DownloadableGroupWatcher *baseMaps READ baseMaps CONSTANT)
-
-  /*! \brief Getter function for the property with the same name
+    /*! \brief Getter function for the property with the same name
 
     @returns Property baseMaps
   */
-  DataManagement::DownloadableGroupWatcher *baseMaps() { return &_baseMaps; };
+    DataManagement::DownloadableGroupWatcher *baseMaps() { return &_baseMaps; };
 
-  /*! \brief Pointer to the DownloadableGroup that holds all data items
-   *
-   *  This is a DownloadableGroup that holds all base maps.
-   */
-  Q_PROPERTY(DataManagement::DownloadableGroupWatcher *databases READ databases CONSTANT)
-
-  /*! \brief Getter function for the property with the same name
+    /*! \brief Getter function for the property with the same name
    *
    *  @returns Property databases
    */
-  DataManagement::DownloadableGroupWatcher *databases() { return &_databases; };
+    DataManagement::DownloadableGroupWatcher *databases() { return &_databases; };
 
-  /*! \brief Describe installed map
+    /*! \brief Describe installed map
      *
      * This method describes installed GeoJSON map files.
      *
@@ -128,72 +153,53 @@ public:
      * @returns A human-readable HTML string, or an empty string if no data is
      * available
      */
-  Q_INVOKABLE static QString describeMapFile(const QString& fileName);
+    Q_INVOKABLE static QString describeMapFile(const QString& fileName);
 
-  /*! \brief Indicates whether the file "maps.json" is currently being downloaded */
-  Q_PROPERTY(bool downloadingGeoMapList READ downloadingGeoMapList NOTIFY downloadingGeoMapListChanged)
-
-  /*! \brief Getter function for the property with the same name
+    /*! \brief Getter function for the property with the same name
 
     @returns Property downloadingGeoMapList
    */
-  bool downloadingGeoMapList() const { return _maps_json.downloading(); };
+    bool downloadingGeoMapList() const { return _maps_json.downloading(); };
 
-  /*! \brief Pointer to group of all geographic maps
-
-    The is a DownloadableGroup that holds all maps.  This is the union of
-    aviation maps and base maps.
-  */
-  Q_PROPERTY(DataManagement::DownloadableGroupWatcher *geoMaps READ geoMaps CONSTANT)
-
-  /*! \brief Getter function for the property with the same name
+    /*! \brief Getter function for the property with the same name
 
     @returns Property geoMaps
   */
-  DataManagement::DownloadableGroupWatcher *geoMaps() { return &_geoMaps; }
+    DataManagement::DownloadableGroupWatcher *geoMaps() { return &_geoMaps; }
 
-  /*! \brief True if the list of available geo maps has already been downloaded */
-  Q_PROPERTY(bool hasGeoMapList READ hasGeoMapList NOTIFY geoMapListChanged)
-  
-  /*! \brief Getter function for the property with the same name
-    
+    /*! \brief Getter function for the property with the same name
+
     @returns hasGeoMapList
    */
-  bool hasGeoMapList() const { return !_geoMaps.downloadables().isEmpty(); }
+    bool hasGeoMapList() const { return !_geoMaps.downloadables().isEmpty(); }
 
-  /*! \brief Current "what's new" message */
-  Q_PROPERTY(QString whatsNew READ whatsNew NOTIFY whatsNewChanged)
-
-  /*! \brief Getter function for property of the same name
+    /*! \brief Getter function for property of the same name
    *
    * @returns Property lastWhatsNew
    */
-  QString whatsNew() const { return _whatsNew; }
+    QString whatsNew() const { return _whatsNew; }
 
-  /*! \brief Hash of the current "what's new" message */
-  Q_PROPERTY(uint whatsNewHash READ whatsNewHash NOTIFY whatsNewChanged)
-
-  /*! \brief Getter function for property of the same name
+    /*! \brief Getter function for property of the same name
    *
    * @returns Property lastWhatsNewHash
    */
-  uint whatsNewHash() const { return qHash(_whatsNew, 0); }
+    uint whatsNewHash() const { return qHash(_whatsNew, 0); }
 
 public slots:
-  /*! \brief Triggers an update of the list of available maps
+    /*! \brief Triggers an update of the list of available maps
 
     This will trigger a download the file maps.json from the remote server.
   */
-  void updateGeoMapList();
+    void updateGeoMapList();
 
 signals:
-  /*! \brief Notification signal for the property with the same name */
-  void geoMapListChanged();
-  
-  /*! \brief Notification signal for the property with the same name */
-  void downloadingGeoMapListChanged();
+    /*! \brief Notification signal for the property with the same name */
+    void geoMapListChanged();
 
-  /*! \brief Download error
+    /*! \brief Notification signal for the property with the same name */
+    void downloadingGeoMapListChanged();
+
+    /*! \brief Download error
 
     This signal is emitted if the download process for the file "maps.json"
     fails for whatever reason.  Since the DataManager updates the list
@@ -202,65 +208,65 @@ signals:
     @param message A brief error message of the form "the requested resource is
     no longer available at the server", possibly translated.
   */
-  void error(QString message);
+    void error(QString message);
 
-  /*! \brief Notification signal for the property with the same name */
-  void whatsNewChanged();
+    /*! \brief Notification signal for the property with the same name */
+    void whatsNewChanged();
 
 private slots:
-  // Trivial method that re-sends the signal, but without the parameter
-  // 'objectName'
-  void errorReceiver(const QString& objectName, QString message);
+    // Trivial method that re-sends the signal, but without the parameter
+    // 'objectName'
+    void errorReceiver(const QString& objectName, QString message);
 
-  // This slot is called when a local file of one of the geo maps changes
-  // content or existence. If the geo map in question is unsupported, it is then
-  // removed. The signals aviationMapListChanged() and
-  // aviationMapUpdatesAvailableChanged() are emitted as appropriate.
-  void localFileOfGeoMapChanged();
+    // This slot is called when a local file of one of the geo maps changes
+    // content or existence. If the geo map in question is unsupported, it is then
+    // removed. The signals aviationMapListChanged() and
+    // aviationMapUpdatesAvailableChanged() are emitted as appropriate.
+    void localFileOfGeoMapChanged();
 
-  // This slot is called when the file "maps.json" is meant to be read.  It is
-  // called by the constructor to interpret an existing "maps.json". It is also
-  // connected to the signal &Downloadable::localFileChanged of
-  // _availableMapsDescription, which is emitted whenever the file "maps.json"
-  // changes in the file system.
-  void readGeoMapListFromJSONFile();
+    // This slot is called when the file "maps.json" is meant to be read.  It is
+    // called by the constructor to interpret an existing "maps.json". It is also
+    // connected to the signal &Downloadable::localFileChanged of
+    // _availableMapsDescription, which is emitted whenever the file "maps.json"
+    // changes in the file system.
+    void readGeoMapListFromJSONFile();
 
-  // This method records the current time as the time when the last update
-  // succeeded, and sets the autoUpdateTimer to check again in one day. This
-  // slot is connected to the signal &Downloadable::localFileChanged of
-  // _availableMapsDescription, which is emitted whenever the file "maps.json"
-  // changes in the file system.
-  void setTimeOfLastUpdateToNow();
+    // This method records the current time as the time when the last update
+    // succeeded, and sets the autoUpdateTimer to check again in one day. This
+    // slot is connected to the signal &Downloadable::localFileChanged of
+    // _availableMapsDescription, which is emitted whenever the file "maps.json"
+    // changes in the file system.
+    void setTimeOfLastUpdateToNow();
 
-  // This method calls 'updateGeoMapList()' if an automatic update is due. It
-  // also sets a reasonable timeout value the timer _autoUpdateTime to fire up
-  // when the next autmatic update is due. It will then start the timer.
-  void autoUpdateGeoMapList();
-  
+    // This method calls 'updateGeoMapList()' if an automatic update is due. It
+    // also sets a reasonable timeout value the timer _autoUpdateTime to fire up
+    // when the next autmatic update is due. It will then start the timer.
+    void autoUpdateGeoMapList();
+
 private:
-  Q_DISABLE_COPY_MOVE(DataManager)
-  
-  // This method returns a list of files in the download directory that have no
-  // corresponding entry in _aviationMaps.
-  QList<QString> unattachedFiles() const;
+    Q_DISABLE_COPY_MOVE(DataManager)
 
-  // The current whats new string from _aviationMaps.
-  QString _whatsNew {};
+    // This method returns a list of files in the download directory that have no
+    // corresponding entry in _aviationMaps.
+    QList<QString> unattachedFiles() const;
 
-  // This timer is used to trigger automatic updates. Its signal QTimer::timeout
-  // is connected to the slot autoUpdateGeoMapList.
-  QTimer _autoUpdateTimer;
+    // The current whats new string from _aviationMaps.
+    QString _whatsNew {};
 
-  // This Downloadable object manages the central text file that describes the
-  // remotely available aviation maps. It is set in the constructor to point to
-  // the URL "https://cplx.vm.uni-freiburg.de/storage/enroute/maps.json"
-  DataManagement::Downloadable _maps_json;
-  
-  // List of geographic maps
-  DataManagement::DownloadableGroup _databases;
-  DataManagement::DownloadableGroup _geoMaps;
-  DataManagement::DownloadableGroup _baseMaps;
-  DataManagement::DownloadableGroup _aviationMaps;
+    // This timer is used to trigger automatic updates. Its signal QTimer::timeout
+    // is connected to the slot autoUpdateGeoMapList.
+    QTimer _autoUpdateTimer;
+
+    // This Downloadable object manages the central text file that describes the
+    // remotely available aviation maps. It is set in the constructor to point to
+    // the URL "https://cplx.vm.uni-freiburg.de/storage/enroute/maps.json"
+    DataManagement::Downloadable _maps_json;
+
+    // List of geographic maps
+    DataManagement::DownloadableGroup _databases;
+    DataManagement::DownloadableGroup _geoMaps;
+    DataManagement::DownloadableGroup _baseMaps;
+    DataManagement::DownloadableGroup _aviationMaps;
 };
 
 };

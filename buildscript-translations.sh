6ae08773f8d12ad3de53c9a8e92ce0278046b161
Stefan Kebekus <kebekus@users.noreply.github.com>
2023-01-14 09:02:07 +0100

    New build system for translations

--- buildscript-translations.sh
+++ buildscript-translations.sh
@@ -2,11 +2,8 @@
 
 cd 3rdParty/enrouteText
 git pull origin master
-lupdate-qt5 ../../src -ts \
-	    assets/enroute_de.ts \
-	    assets/enroute_fr.ts \
-	    assets/enroute_it.ts \
-	    assets/enroute_pl.ts \
-	    assets/enroute_pt.ts
+cd ../../build-linux-qt6-debug
+ninja update_translations
+cd ../3rdParty/enrouteText
 git commit -a -m "Update translations"
 git push origin HEAD:master

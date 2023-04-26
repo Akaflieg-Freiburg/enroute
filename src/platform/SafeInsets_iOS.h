e6cc43f6d1a1291676afc9ee93b19c004ae93def
Stefan Kebekus <kebekus@users.noreply.github.com>
2023-04-26 19:00:46 +0200

    Add file

---
+++ src/platform/SafeInsets_iOS.h
@@ -0,0 +1,58 @@
+/***************************************************************************
+ *   Copyright (C) 2019-2022 by Stefan Kebekus                             *
+ *   stefan.kebekus@gmail.com                                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#pragma once
+
+#include <QQmlEngine>
+
+#include "SafeInsets_Abstract.h"
+
+namespace Platform {
+
+/*! \brief Implementation of SafeInsets for Linux desktop devices, as a QML singleton */
+
+class SafeInsets : public SafeInsets_Abstract
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+
+    // Properties need to be repeated, or else the Qt CMake macros cannot find them.
+    Q_PROPERTY(double bottom READ bottom NOTIFY bottomChanged)
+    Q_PROPERTY(double left READ left NOTIFY leftChanged)
+    Q_PROPERTY(double right READ right NOTIFY rightChanged)
+    Q_PROPERTY(double top READ top NOTIFY topChanged)
+    Q_PROPERTY(double wHeight READ wHeight NOTIFY wHeightChanged)
+    Q_PROPERTY(double wWidth READ wWidth NOTIFY wWidthChanged)
+
+public:
+    /*! \brief Standard constructor
+     *
+     * @param parent Standard QObject parent pointer
+     */
+    explicit SafeInsets(QObject *parent = nullptr);
+
+    ~SafeInsets() override = default;
+
+private:
+    Q_DISABLE_COPY_MOVE(SafeInsets)
+};
+
+} // namespace Platform

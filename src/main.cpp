c59a891ebad3be6506d5a4b7e148f7fae9198fb4
Stefan Kebekus <kebekus@users.noreply.github.com>
2023-05-03 18:24:06 +0200

    Choose style

--- src/main.cpp
+++ src/main.cpp
@@ -184,6 +184,10 @@ auto main(int argc, char *argv[]) -> int
      * Set up ApplicationEngine for QML
      */
 
+#if defined(Q_OS_ANDROID) or defined(Q_OS_IOS)
+    QQuickStyle::setStyle("Material");
+#endif
+
     auto* engine = new QQmlApplicationEngine();
     engine->rootContext()->setContextProperty(QStringLiteral("manual_location"), MANUAL_LOCATION );
     engine->rootContext()->setContextProperty(QStringLiteral("global"), new GlobalObject(engine) );

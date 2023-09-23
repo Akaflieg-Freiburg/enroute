538ef1d114009cd6124dd2304b52d7c5371a1bfc
kebekus <kebekus@users.noreply.github.com>
2023-09-23 13:24:04 +0000

    Deploying to gh-pages from @ Akaflieg-Freiburg/enroute@2545c33eb961668cf8e2203572c2e6e25403ca0d 🚀

--- APIdoc/menudata.js
+++ APIdoc/menudata.js
@@ -54,6 +54,7 @@ var menudata={children:[
 {text:"u",url:"functions_u.html#index_u"},
 {text:"v",url:"functions_v.html#index_v"},
 {text:"w",url:"functions_w.html#index_w"},
+{text:"z",url:"functions_z.html#index_z"},
 {text:"~",url:"functions_~.html#index__7E"}]},
 {text:"Functions",url:"functions_func.html",children:[
 {text:"a",url:"functions_func.html#index_a"},
@@ -78,6 +79,7 @@ var menudata={children:[
 {text:"u",url:"functions_func_u.html#index_u"},
 {text:"v",url:"functions_func_v.html#index_v"},
 {text:"w",url:"functions_func_w.html#index_w"},
+{text:"z",url:"functions_func_z.html#index_z"},
 {text:"~",url:"functions_func_~.html#index__7E"}]},
 {text:"Variables",url:"functions_vars.html"},
 {text:"Enumerations",url:"functions_enum.html"},

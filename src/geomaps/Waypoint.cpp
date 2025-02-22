1d29f7420df45acb0a570a770a89a8034367cad9
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-02-22 10:28:47 +0100

    Update Waypoint.cpp

--- src/geomaps/Waypoint.cpp
+++ src/geomaps/Waypoint.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2019-2024 by Stefan Kebekus                             *
+ *   Copyright (C) 2019-2025 by Stefan Kebekus                             *
  *   stefan.kebekus@gmail.com                                              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -93,7 +93,9 @@ GeoMaps::Waypoint::Waypoint(const QJsonObject &geoJSONObject)
         m_coordinate.setAltitude(properties[QStringLiteral("ELE")].toDouble());
     }
 
-    // If the file was not created by Enroute, then the property array will not conform to the specification here: https://github.com/Akaflieg-Freiburg/enrouteServer/wiki/GeoJSON-files-used-in-enroute-flight-navigation
+    // If the file was not created by Enroute, then the property array will not
+    // conform to the specification here:
+    // https://github.com/Akaflieg-Freiburg/enrouteServer/wiki/GeoJSON-files-used-in-enroute-flight-navigation
     // We create a fake entry instead.
     if (!m_properties.contains(QStringLiteral("TYP")))
     {

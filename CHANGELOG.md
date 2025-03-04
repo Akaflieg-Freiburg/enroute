cbf2f3d230742412eb231a3309c0d388b4b1ffb8
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-03-04 07:34:58 +0100

    Cleanup, address #503

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,14 @@
 # Changelog
 
-## [2.32.14] - 2025-03-xx
+## [2.32.14] - 2025-03-04
 
 ### Fixed
 - Fix problem with the file picker dialog on Android, where files from external
   sources were not opened. (#491)
+
+### Changed
+- Increase distance threshold where aircraft is considered to be on-route from 3
+  to 5 NM. (#503)
 
 
 ## [2.32.13] - 2025-03-02
--- src/platform/FileExchange_Android.h
+++ src/platform/FileExchange_Android.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2019-2023 by Stefan Kebekus                             *
+ *   Copyright (C) 2019-2025 by Stefan Kebekus                             *
  *   stefan.kebekus@gmail.com                                              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -72,8 +72,18 @@ public:
      */
     QString viewContent(const QByteArray& content, const QString& mimeType, const QString& fileNameTemplate) override;
 
-#warning
-    Q_INVOKABLE static void openFilePicker(const QString& mime);
+    /*! \brief Opens the native Android file picker
+     *
+     *  This method facilitates a workaround against
+     *  https://bugreports.qt.io/browse/QTBUG-118154 which renders the file
+     *  dialog effectively unusable under Android. This method returns
+     *  immediately. It display the file picker on top of the app. Once a file
+     *  is chosen, the standart import mechanism is invoked.
+     *
+     *  @param mimeType A space-separated list of mime types for that that
+     *  should be selectable
+     */
+    Q_INVOKABLE static void openFilePicker(const QString& mimeType);
 
 public slots:
     /*! \brief Implements pure virtual method from FileExchange_Abstract */

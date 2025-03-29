eaabb14bf61fc53f6ec13b34a219361a830cbc15
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-03-29 07:09:00 +0100

    Update Librarian.cpp

--- src/Librarian.cpp
+++ src/Librarian.cpp
@@ -307,7 +307,7 @@ auto Librarian::getStringFromRessource(const QString &name) -> QString
                   + "</p>";
 #if !defined(Q_OS_IOS)
         result += "<p>"
-                  + tr("Due to a change in Google's policies, it is no longer possible to share locations from Google Maps with Enroute Flight Navigation.") +
+                  + tr("Due to a change in Google's policies, it is no longer possible to share locations from the apps 'Google Maps' and 'Google Maps Go'″ with Enroute Flight Navigation.") +
                   + "</p>";
         result += "<p>"
                   + tr("<strong>Technology Preview:</strong> Enroute Flight Navigation is now able to connect to traffic data receivers via Bluetooth Low Energy. "

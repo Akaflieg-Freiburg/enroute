13e3abfa7ed081aa2c10bd245afbd060b15ab100
Charly Lima <CharlyLima@github>
2025-04-01 15:07:43 +0200

    add menu entry to activate OGN in ConnectionManager.

--- src/traffic/TrafficDataProvider.cpp
+++ src/traffic/TrafficDataProvider.cpp
@@ -70,7 +70,7 @@ Traffic::TrafficDataProvider::TrafficDataProvider(QObject *parent)
     addDataSource( new Traffic::TrafficDataSource_Tcp(true, QStringLiteral("192.168.42.1"), 2000, this) );
     addDataSource( new Traffic::TrafficDataSource_Udp(true, 4000, this) );
     addDataSource( new Traffic::TrafficDataSource_Udp(true, 49002, this));
-    addDataSource( new Traffic::TrafficDataSource_Ogn(true, "", 0, this));
+    //addDataSource( new Traffic::TrafficDataSource_Ogn(true, "", 0, this));
 
     // Setup Bindings
     m_pressureAltitude.setBinding([this]() {return computePressureAltitude();});

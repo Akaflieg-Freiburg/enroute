77996bd965207a044ddaf2a37747b1f05a9747bc
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-12-04 20:28:00 +0100

    Working...

--- src/platform/FileExchange_iOS.cpp
+++ src/platform/FileExchange_iOS.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2019-2020 by Stefan Kebekus                             *
+ *   Copyright (C) 2019-2025 by Stefan Kebekus                             *
  *   stefan.kebekus@gmail.com                                              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -19,7 +19,7 @@
  ***************************************************************************/
 
 #include "platform/FileExchange_iOS.h"
-#include "platform/PlatformAdaptor_Abstract.h"
+#include "platform/PlatformAdaptor.h"
 
 #include "ios/ObjCAdapter.h"
 #include "qfileinfo.h"
